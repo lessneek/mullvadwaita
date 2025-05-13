eb094c2b81848a640c0d2cc0d30eaa518bded8c2
lessneek <1essn33k@gmail.com>
2025-05-13 15:45:37 +0300

    Change deprecated adw::AboutWindow to adw::AboutDialog

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -3,9 +3,8 @@ use crate::icon_names;
 use adw::prelude::*;
 use relm4::prelude::*;
 
-pub fn show_about_dialog(root: &impl IsA<gtk::Window>) {
-    let dialog = adw::AboutWindow::builder()
-        .icon_name(icon_names::BACKGROUND_APP_GHOST)
+pub fn show_about_dialog(root: &impl IsA<gtk::Widget>) {
+    let dialog = adw::AboutDialog::builder()
         .application_icon(icon_names::BACKGROUND_APP_GHOST)
         .application_name("Mullvadwaita")
         .developer_name("Lessneek")
@@ -15,10 +14,8 @@ pub fn show_about_dialog(root: &impl IsA<gtk::Window>) {
         .website("https://github.com/lessneek/mullvadwaita")
         .issue_url("https://github.com/lessneek/mullvadwaita/issues")
         .version(env!("CARGO_PKG_VERSION"))
-        .modal(true)
-        .transient_for(root)
-        .developers(vec!["Lessneek", "aiska"])
+        .developers(vec!["Lessneek"])
         .comments("Mullvad VPN daemon controller.")
         .build();
-    dialog.present();
+    dialog.present(Some(root));
 }
